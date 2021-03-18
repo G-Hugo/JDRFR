5487d5f5ea744daa52e5f03717835797952c253e
Hugo Gaveau <gaveau.hugo@gmail.com>
2021-03-18 15:49:18 +0100

    Les modifs que je voulais faire 3.1

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -126,7 +126,6 @@ export class SimpleActorSheet extends ActorSheet {
 
     // Rollable Buttons
     html.find(".characteristic-roll").click(this._onClickCharacteristic.bind(this));
-    html.find(".skill-roll").click(this._onSkillRoll.bind(this));
     html.find(".magic-skill-roll").click(this._onMagicSkillRoll.bind(this));
     html.find(".combat-roll").click(this._onCombatRoll.bind(this));
     html.find(".magic-roll").click(this._onSpellRoll.bind(this));
--- module/simple.js
+++ module/simple.js
@@ -33,13 +33,13 @@ Hooks.once("init", async function() {
 
   // Register sheet application classes
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("dnd5e", SimpleActorSheet, {types: ["character"], makeDefault: true});
+  Actors.registerSheet("uesrpg-d100-modifie", SimpleActorSheet, {types: ["character"], makeDefault: true});
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("uesrpg-d100", SimpleItemSheet, {types: ["item", "armor", "weapon", "spell", "trait", "talent", "power", "combatStyle", "ammunition"], makeDefault: true});
-  Actors.registerSheet("uesrpg-d100", npcSheet, {types: ["npc"], makeDefault: true});
+  Items.registerSheet("uesrpg-d100-modifie", SimpleItemSheet, {types: ["item", "armor", "weapon", "spell", "trait", "talent", "power", "combatStyle", "ammunition"], makeDefault: true});
+  Actors.registerSheet("uesrpg-d100-modifie", npcSheet, {types: ["npc"], makeDefault: true});
 
   // Register system settings
-  game.settings.register("uesrpg-d100", "legacyUntrainedPenalty", {
+  game.settings.register("uesrpg-d100-modifie", "legacyUntrainedPenalty", {
     name: "Legacy Untrained Penalty",
     hint: "Checking this option enables the UESRPG v2 penalty for Untrained skills at -20 instead of the standard -10. Must refresh the client manually (F5) after selecting this option to see the changes on actor sheets.",
     scope: "world",
