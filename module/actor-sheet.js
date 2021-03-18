d8bbf0cb5eee58e1816398ff50d0979f73625104
Hugo Gaveau <gaveau.hugo@gmail.com>
2021-03-18 15:34:32 +0100

    Les modifs que je voulais faire 3.0

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -8,7 +8,7 @@ export class SimpleActorSheet extends ActorSheet {
 	static get defaultOptions() {
 	  return mergeObject(super.defaultOptions, {
   	  classes: ["worldbuilding", "sheet", "actor"],
-  	  template: "systems/uesrpg-d100/templates/actor-sheet.html",
+  	  template: "systems/uesrpg-d100-modifie/templates/actor-sheet.html",
       width: 600,
       height: 600,
       tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
